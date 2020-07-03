3cc03710e24f454e6d29a60db0de7ded6be74450
sekoudosso82 <sekou.dosso82@gmail.com>
2020-07-03 08:58:59 -0400

    day 17  commit

---
+++ hackerRank30DaysCode/day17MoreExceptionTryCatch.js
@@ -0,0 +1,30 @@
+// Task
+// Write a Calculator class with a single method: 
+// int power(int,int). The power method takes two integers, 
+// n and p, as parameters and returns the integer result of n^p.
+// If either n or p is negative, then the method must throw an
+// exception with the message: n and p should be non-negative. 
+
+class Calculator {
+    //var
+    // contructor
+    // constructor(num, pow){
+    //     this.num = num
+    //     this.pow = pow
+    // }
+    // methods
+     power(n,p){
+        try{
+            if (n < 0) throw "n and p should be non-negative"
+            if (p < 0) throw "n and p should be non-negative"
+            // if ( p === 0 && n > 0) throw 1
+            return Math.pow(n, p);
+            
+        } catch( error){
+            // console.log(error)
+            // console.log("n and p should be non-negative")
+            return error
+        }
+
+     }
+}

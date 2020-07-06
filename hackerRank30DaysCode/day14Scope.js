95e93365443c7828461eda1d221f08eb63d5f6d2
sekoudosso82 <sekou.dosso82@gmail.com>
2020-07-06 18:17:54 -0400

    day 13  commit

---
+++ hackerRank30DaysCode/day14Scope.js
@@ -0,0 +1,20 @@
+// The absolute difference between two integers, a and b, 
+// is written as |a - b|. The maximum absolute difference 
+// between two integers in a set of positive integers, elements, 
+// is the largest absolute difference between any two integers 
+// in elelements. 
+// The Difference class is started for you in the editor. 
+// It has a private integer array ( elements) for storing 
+// non-negative integers, and a public integer (maximumDifference)
+//  for storing the maximum absolute difference.
+
+// Task
+// Complete the Difference class by writing the following:
+
+    
+// . A class constructor that takes an array of integers as 
+// a parameter and saves it to the elements  instance variable.
+
+// . A computeDifference method that finds the maximum absolute 
+// difference between any 2 numbers in N and stores it in 
+// the  maximumuDifference instance variable. 

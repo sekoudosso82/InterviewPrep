e565c24fd4dfcc7174eac40bddd11608abebe282
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-21 18:19:33 -0400

    bash sed learning

--- bash_learning/arrayyy.sh
+++ bash_learning/arrayyy.sh
@@ -92,3 +92,32 @@ for var in ${array[*]}; do
         echo $var
     fi
 done
+
+
+# CONCATENATE ARRAY 
+# Given a list of countries, each on a new line, your task is to read 
+# them into an array. Then, concatenate the array with itself (twice) - so 
+# that you have a total of three repetitions of the original array - and 
+# then display the entire concatenated array, with a space between each of 
+# the countries' names.
+
+# Input Format: A list of country names. The only characters present in the 
+# country names will be upper or lower case characters and hyphens.
+
+# Output Format: Display the entire concatenated array, with a space between 
+# each of them.
+
+
+
+for line in `cat`; do
+    countries=( "${countries[@]}" $line )
+done
+
+countries=("${countries[@]}" "${countries[@]}" "${countries[@]}")
+echo "${countries[@]}"
+
+or 
+array=($(cat))
+Total=("${array[@]}" "${array[@]}" "${array[@]}")
+echo ${Total[@]}
+

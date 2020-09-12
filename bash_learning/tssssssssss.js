b80db4619ebbca7010f02a2b0db1430544a3e5a9
sekoudosso82 <sekou.dosso82@gmail.com>
2020-09-11 22:51:43 -0400

    hackerRank react code_challenge commit

---
+++ bash_learning/tssssssssss.js
@@ -0,0 +1,15 @@
+const sayHello = name => {
+    // You can print to STDOUT for debugging like you normally would:
+  //   console.log("Hello, ",name,"!");
+    
+    // but you need to return the value in order to complete the challenge  
+    if (name){
+      return name;
+    }
+    else{
+      return "Hello there!";
+    }
+     // TODO: return the correct value
+  };
+
+  sayHello("sekou")

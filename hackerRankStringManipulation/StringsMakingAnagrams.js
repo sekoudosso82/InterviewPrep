8a3c1c47795d4d3a55c76c01796991078273dd56
sekoudosso82 <sekou.dosso82@gmail.com>
2020-09-20 11:55:15 -0400

    string manipulation alternating characters code challenge

--- hackerRankStringManipulation/StringsMakingAnagrams.js
+++ hackerRankStringManipulation/StringsMakingAnagrams.js
@@ -1,30 +1,30 @@
-Alice is taking a cryptography class and finding anagrams to be very useful. 
-We consider two strings to be anagrams of each other if the first string's 
-letters can be rearranged to form the second string. In other words, both 
-strings must contain the same exact letters in the same exact frequency 
-For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+// Alice is taking a cryptography class and finding anagrams to be very useful. 
+// We consider two strings to be anagrams of each other if the first string's 
+// letters can be rearranged to form the second string. In other words, both 
+// strings must contain the same exact letters in the same exact frequency 
+// For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
 
-Alice decides on an encryption scheme involving two large strings where 
-encryption is dependent on the minimum number of character deletions 
-required to make the two strings anagrams. Can you help her find this number?
+// Alice decides on an encryption scheme involving two large strings where 
+// encryption is dependent on the minimum number of character deletions 
+// required to make the two strings anagrams. Can you help her find this number?
 
-Given two strings, a  and b, that may or may not be of the same length, 
-determine the minimum number of character deletions required to make 
-a and b anagrams. Any characters can be deleted from either of the strings.
+// Given two strings, a  and b, that may or may not be of the same length, 
+// determine the minimum number of character deletions required to make 
+// a and b anagrams. Any characters can be deleted from either of the strings.
 
-For example, if a = cde and b = dcf, we can delete e from string a and f 
-from string b so that both remaining strings are cd and dc which are anagrams. 
+// For example, if a = cde and b = dcf, we can delete e from string a and f 
+// from string b so that both remaining strings are cd and dc which are anagrams. 
 
-Function Description
+// Function Description
 
-Complete the makeAnagram function in the editor below. It must return 
-an integer representing the minimum total characters that must be 
-deleted to make the strings anagrams.
+// Complete the makeAnagram function in the editor below. It must return 
+// an integer representing the minimum total characters that must be 
+// deleted to make the strings anagrams.
 
-makeAnagram has the following parameter(s):
+// makeAnagram has the following parameter(s):
 
-    a: a string
-    b: a string
+//     a: a string
+//     b: a string
 
 function makeAnagram(a, b) {
     let as = a.split('')

82ce7bed89cd22a4f7a9bc3fbe482c0b184b8899
sekoudosso82 <sekou.dosso82@gmail.com>
2020-09-20 10:07:28 -0400

    string anagrams code challenge

--- hackerRankStringManipulation/StringsMakingAnagrams.js
+++ hackerRankStringManipulation/StringsMakingAnagrams.js
@@ -58,3 +58,25 @@ function makeAnagram(a, b) {
     return count
 
 }
+
+// second  solution 
+
+function main() {
+    var a = readLine().split("");
+    var b = readLine().split("");
+var countA = a.length;
+var countB = b.length;
+var count = countA + countB;
+    var pair = 0;
+    for(var i = 0; i < countA;i++){
+        for(var j = 0; j < countB; j++){
+            if(a[i] == b[j]){
+                b.splice(j,1);
+                pair++;
+                break;
+            }
+        }
+    }
+    console.log(count - (pair*2));
+}
+
